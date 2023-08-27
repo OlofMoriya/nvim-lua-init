245cb0460b5657fbbe817e7adc408d6b3ad61885
OlofMoriya <olof.moriya@gmail.com>
2023-08-27 23:17:13 +0200

    fixes autocomplete again

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,10 @@
 return {
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'hrsh7th/nvim-cmp' },
     {
         'vonheikemen/lsp-zero.nvim',
+        lazy = false,
         dependencies = {
             -- lsp support
             { 'neovim/nvim-lspconfig' },
@@ -20,6 +24,23 @@ return {
             { 'l3mon4d3/luasnip' },
             { 'rafamadriz/friendly-snippets' },
             { "solidjs-community/solid-snippets" }
-        }
+        },
+        config = function() 
+            vim.keymap.set("n", "<leader>f", ":LspZeroFormat<CR>")
+
+            local lsp = require('lsp-zero')
+
+            lsp.preset('recommended')
+
+            -- vim.keymap.set({ "i", "s" }, "<C-Space>", function() require 'luasnip'.jump(1) end, { desc = "LuaSnip forward jump" })
+            -- vim.keymap.set({ "i", "s" }, "<S-Space>", function() require 'luasnip'.jump(-1) end, { desc = "LuaSnip backward jump" })
+
+            local nvim_lsp = require('lspconfig')
+            lsp.configure('csharp_ls', {
+                root_dir = nvim_lsp.util.root_pattern('*.sln', '.git')
+            })
+
+            lsp.setup()
+        end
     },
 }
