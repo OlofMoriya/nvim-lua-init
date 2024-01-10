3f8c0fdba3a502eb5522d264f8c93435690ac4ee
Olof Moriya <olof.moriya@gmail.com>
2024-01-10 13:56:41 +0100

    No notification with lazy

--- init.lua
+++ init.lua
@@ -14,7 +14,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup(
+    {
+        spec = "plugins",
+        change_detection = { notify = false }
+    }
+)
 
 
 require("setup")
